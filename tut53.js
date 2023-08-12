f10b60ea20944c7c6efbe76750891e168b1d7b3f
Tamur Hussain <140755726+mtamur180@users.noreply.github.com>
2023-08-12 19:57:24 +0500

    Add files via upload

---
+++ tut53.js
@@ -0,0 +1,56 @@
+// let name ="hamid";
+// let name1 ="tamur";
+// let name2= "zain";
+// let name3 ="jarral";
+
+// console.log( name1  +" is a good boy"); 
+// console.log( name2 +" is a good boy"); 
+// console.log( name3 +" is a good boy"); 
+// console.log( name +" is a good boy"); 
+
+
+// its simple function 
+//  function greet(name){
+
+//     console.log(name+" is a good boy")
+//  }
+//  let name ="hamid"
+//  let name2="zain"
+//  let name3="abdullah"
+//  let name4="haji haseeb"
+//  let greetText=" good morning"
+//  greet(name)
+//  greet(name2)
+//  greet(name3)
+//  greet(name4)
+
+
+// its multiple function
+//  function greet(name,greetText){
+//      console.log(name+greetText)
+//     console.log(name+" is a good boy")
+//  }
+//  let name ="hamid"
+//  let name2="zain"
+//  let name3="abdullah"
+//  let name4="haji haseeb"
+//  let greetText=" good morning"
+//  greet(name,greetText)
+//  greet(name2,greetText)
+//  greet(name3,greetText)
+//  greet(name4,greetText)
+
+
+function greet(name,text="its default"){
+    console.log(name,text)
+}
+let name= "hamid";
+greet(name)
+
+
+
+ function sum(math,science,urdu){
+    console.log(math+science+urdu)
+ }
+ let TotalMarks= sum(80,79,93);
+ 
